fafa9eefcaf169929cbb4cbd43574deb2d0c25c7
MICHAEL JAMES <97917001+MJCappella@users.noreply.github.com>
2024-03-25 13:12:07 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,13 @@
-# Install package
-package { 'puppet-lint':
-  ensure   => '2.1.0',
-  provider => 'gem',
+# Install Flask version 2.1.0 using pip3
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install Flask==2.1.0',
+  path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  refreshonly => true,
+  unless      => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  require     => Package['python3-pip'],
 }
